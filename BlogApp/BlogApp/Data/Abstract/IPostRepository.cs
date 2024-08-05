ad17f4fc8396d42bc922eb0de49195163d88fa5b
Yusuf Aydın <93652827+yayd1n@users.noreply.github.com>
2024-08-05 14:04:24 +0300

    BlogApp
    Sadık Turanın Udemyde Asp.net Core eğitimden bakarak yapılmışıtır.

---
+++ BlogApp/BlogApp/Data/Abstract/IPostRepository.cs
@@ -0,0 +1,13 @@
+
+using BlogApp.Entity;
+
+namespace BlogApp.Data.Abstract
+{
+    public interface IPostRepository
+    {
+        IQueryable<Post> Posts { get; }
+        void CreatePost(Post post);
+        void EditPost(Post post);
+        void EditPost(Post post, int[] tagsIds);
+    }
+}
